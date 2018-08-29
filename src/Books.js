b307d941fec951f2b5aa2e627602ff1b9a9516a0
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-08-29 15:30:15 -0400

    refactor 1.0

--- src/Books.js
+++ src/Books.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 
 class Books extends Component {
   render() {
-      let fetchedThumbnail = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : '';
    	return(
       	<div className="book">
                           <div className="book-top">
                               <div className="book-cover" style={{ width: 128, height: 193,
-                                   backgroundImage: `url("${fetchedThumbnail}")` }}></div>
+                                   backgroundImage: `url("${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''}")` }}></div>
                             <div className="book-shelf-changer">
                               <select onChange={(e) =>
                                   this.props.changeShelf(
